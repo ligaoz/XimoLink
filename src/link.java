ccb8601400eca053001c6efbb7487b7c2e36d052
ligaoz <liga.ozolina@ucdconnect.ie>
2017-08-26 21:59:33 +0100

    XimoStudio exetion upon XimoLink start added

--- src/link.java
+++ src/link.java
@@ -3,61 +3,74 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class link {
+    private PrintWriter output = null;
+    private BufferedReader input = null;
+    private Socket LinkSocket;
+    private String inputLine;
+    private String path;
 
     public static void main(String[] args) throws IOException {
-        PrintWriter output = null;
-        BufferedReader input = null;
-        Socket LinkSocket;
-        String inputLine;
 
-        //Check if XimoStudio is running if its not start the program
-      /*  if(CheckXimoStudio.isRunning()){
-            System.out.println("Two instances of this program cannot " +
-                        "be running at the same time.  Exiting now");
-        }
-        else{
-            CheckXimoStudio.onStart();
-            CheckXimoStudio.epicHeavyWorkGoesHere();
-            CheckXimoStudio.onFinish();
-        }
-*/
-        //Start to communicate with XimoStudio
-        try {
-            LinkSocket = new Socket("127.0.0.1", 2525); //Opening a socket
-            System.out.println("Connected to " + LinkSocket.getInetAddress().getHostName());
+        link link = new link();
 
-            input = new BufferedReader(new InputStreamReader(LinkSocket.getInputStream()));//Opening data input stream to receive response from XimoStudio
-            output = new PrintWriter(LinkSocket.getOutputStream(), true);//Opening data output stream to send command
+        //Start the Ximo Studio
+       /* try {
+            // print a message
+            System.out.println("Executing XimoStudio");
 
-            System.out.println("Started talking");
-            output.println("pasteClipboard");//send command to XimoStudio
-            System.out.println("Command paste from clipboard sent");
+            // create a process and execute Ximo Studio
+            Process process = Runtime.getRuntime().exec(new String[]{"java", "-jar", "C:\\Users\\i340902\\git\\XimoStudio\\download\\XimoStudio_v0.8.0.jar"});
 
-            while (( inputLine = input.readLine()) != null){
-                //read input from server
-                System.out.println("Server: " + inputLine);
-
-                if (inputLine.equals("OK")) { //acknowledgement for paste from clipboard
-                    System.out.println("Ack from XimoStudio received");
-                }else if(inputLine.equals("ready")) { //Wait until command "ready" is received from XimoStudio
-                    System.out.println("Results copied to clipboard\n Exiting XimoLink");
-                    //do a clean up
-                    try {
-                        output.close();// close the output stream
-                        input.close();// close the input stream
-                        LinkSocket.close();// close the socket
-                        System.exit(0);
-                    }
-                    catch (IOException e) {
-                        System.out.println(e);
-                    }
-                }else
-                    System.out.println("Unknown command ");
+            if (process.isAlive()) {
+                System.out.println("Ximo is alive");
+                serverCommunication(link);
+                process.destroy();//kill XimoLink
             }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println(e);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }     */
+    }
+     public link() throws IOException {
+            this.connect();
+            this.send();
+            this.listen();
+     }
+    private void connect() throws UnknownHostException, IOException {
+        //Connect to XimoStudio
+        //Opening a socket
+        LinkSocket = new Socket("127.0.0.1", 2525);
+        System.out.println("Connected to " + LinkSocket.getInetAddress().getHostName());
+        //Opening data input stream to receive response from XimoStudio
+        input = new BufferedReader(new InputStreamReader(LinkSocket.getInputStream()));
+        //Opening data output stream to send command
+        output = new PrintWriter(LinkSocket.getOutputStream(), true);
+        System.out.println("Connection is open");
+    }
+    private void listen() throws IOException {
+        //listen to server
+        while ((inputLine = input.readLine()) != null) {
+            //read input from server
+            System.out.println("Server: " + inputLine);
+            if (inputLine.equals("OK")) { //acknowledgement from XimoStudio for pasting SQL from clipboard
+                System.out.println("Ack from XimoStudio received");
+            } else if (inputLine.equals("ready")) { // XimoStudio has copied results to clipboard
+                System.out.println("Results were copied to clipboard\nExiting XimoLink");
+                cleanUp();
+            } else {
+                System.out.println("Unknown command ");
+            }
         }
+    }
+    private void cleanUp() throws IOException {
+        //do a clean up
+        output.close();// close the output stream
+        input.close();// close the input stream
+        LinkSocket.close();// close the socket
+        System.exit(0);
+    }
+    private void send() {
+        //send command to XimoStudio
+        output.println("pasteClipboard");
+        System.out.println("Command paste from clipboard sent");
     }
 }
